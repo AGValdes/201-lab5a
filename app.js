e15fa93e863c2315bb26511774d2228dc4e6e930
Ameilia Valdes <valdes.ameilia@gmail.com>
2020-10-02 16:18:35 -0700

    solved 4th question

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ Write a function called sum() that takes in two numbers as arguments and then re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
 // Write your code here
-function sum(a, b) { //eslint-disable-line
-  var totalSum = a + b;
+function sum(a, b, c=0) { //eslint-disable-line
+  var totalSum = a + b + c;
   var string = 'The sum of ' + a + ' and ' + b + ' is ' + totalSum + '.';
   return [totalSum, string];
 }
@@ -28,14 +28,14 @@ Write a function called multiply() that takes in two numbers as arguments and re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
 // Write your code here
-function multiply(a, b) { 
-    var product = a*b;
+function multiply(a, b, c=1) { 
+    var product = a*b*c;
     var string = 'The product of ' + a + ' and ' + b + ' is ' + product+ '.';
     return [product, string];
 }
 
 // Here is the test for multiply(); uncomment it to run it
-testMultiply(5,9);
+// testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -52,7 +52,15 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-
+  var sumNum = sum(a,b,c)[0];
+  console.log(sumNum);
+  var prodNum = multiply(a,b,c)[0];
+  console.log(prodNum);
+  var stringOne = a + ' and ' + b + ' and ' + c + ' sum to ' + sumNum + '.';
+  console.log(stringOne);
+  var stringTwo = 'The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + prodNum + '.';
+  console.log(stringTwo);
+  return [sumNum, prodNum, stringOne, stringTwo];
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
@@ -71,15 +79,19 @@ IMPORTANT DETAIL: You may not use the arithmetic operator + in this function. To
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumArray() function and see if the test passes.*/
 
 // Write your code here
-var testArray = [2, 3, 4]; //eslint-disable-line
+var testArray = [2, 3, 4];
 
-function sumArray(sumArr) { //eslint-disable-line
-
+function sumArray(sumArr) { 
+  var numSum = sum(testArray[0],testArray[1], testArray[2])[0];
+  console.log(numSum);
+  var string = testArray + ' was passed in as an array of numbers, and ' + numSum + ' is their sum.'
+  console.log(string);
+  return [numSum,string]
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
